c691c6be8ce40d685d8fd743c26270220526801b
Tim Millwood <tim@millwoodonline.co.uk>
2014-01-27 16:21:40 +0000

    making more simple

--- Gemfile
+++ Gemfile
@@ -1,8 +1,6 @@
 source 'https://rubygems.org'
 ruby "2.1.0"
 
-gem 'unicorn'
 gem 'sinatra'
 gem 'sinatra-assetpack'
 gem 'sass'
-gem 'bootstrap-sass'
--- app.rb
+++ app.rb
@@ -1,23 +1,14 @@
 require 'sinatra/base'
 require 'sinatra/assetpack'
 require 'sass'
-require 'bootstrap-sass'
 
 
 class App < Sinatra::Base
-  path = File.expand_path(File.dirname(__FILE__))
-  set :root, '/'
-  set :views, "#{path}/views"
   register Sinatra::AssetPack
 	assets do
-   serve '/css', from: "#{path}/css"
-   serve '/bootstrapjs', from: "#{Bootstrap.javascripts_path}/bootstrap"
-
     css :main, [
+     '/css/reset.css',
      '/css/*.css'
-    ]
-    js :main, [
-    '/bootstrapjs/*.js'
     ]
     css_compression :sass
   end
